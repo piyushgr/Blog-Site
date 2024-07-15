744ff64f77fcdd4a65d96324a4eda405f405a859
Piyush Gaur <121219215+piyushgr@users.noreply.github.com>
2024-07-15 17:59:44 +0530

    added google auth

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,21 +1,29 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
+import themeReducer from "./theme/themeSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import persistStore from "redux-persist/es/persistStore";
 
-const authSlice = createSlice({
-  name: "auth",
-  initialState: {
-    isLogin: false,
-  },
-  reducers: {
-    login(state) {
-      state.isLogin = true;
-    },
-    logout(state) {
-      state.isLogin = false;
-    },
-  },
+const rootReducer = combineReducers({
+  user: userReducer,
+  theme: themeReducer,
 });
-export const authActions = authSlice.actions;
+
+const persistConfig = {
+  key: "root",
+  storage,
+  version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: authSlice.reducer,
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
